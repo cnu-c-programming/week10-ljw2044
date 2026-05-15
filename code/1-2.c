49827a9e3a2a5da61612f16308e79f09c498afd3
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-05-15 09:25:22 +0900

    1-2c

--- code/1-2.c
+++ code/1-2.c
@@ -3,7 +3,7 @@
 
 struct IPv4 {
     unsigned int version;
-    unsigned int ihi;
+    unsigned int ihl;
     unsigned long long ttl;
     unsigned long long protocol;
     unsigned char data[1024];
@@ -11,15 +11,15 @@ struct IPv4 {
 
 int main() {
     struct IPv4 s;
-    s.version;
-    s.ihi;
-    s.ttl;
-    s.protocol;
+    s.version = 0;
+    s.ihl = 0;
+    s.ttl = 256;
+    s.protocol = 0;
     sprintf(s.data, "hello world");
 
     printf("version: %u, size: %zu\n", s.version, sizeof(s.version));
-    printf("ihi: %u, size: %zu\n", s.ihi, sizeof(s.ihi));
+    printf("ihl: %u, size: %zu\n", s.ihl, sizeof(s.ihl));
     printf("ttl: %llu, size: %zu\n", s.ttl, sizeof(s.ttl));
     printf("protocol: %llu, size: %zu\n", s.protocol, sizeof(s.protocol));
     printf("data: %s, size: %zu\n", s.data, sizeof(s.data));
-}
+}    
